4319f33642fd0bff04e1cfb3f10f5ce490616a19
l31tu <70357291+l31tu@users.noreply.github.com>
2025-09-04 20:06:08 +0200

    Placeholder

---
+++ main.js
@@ -0,0 +1,44 @@
+//Popups
+const toggleVTag = document.querySelector(".toggle-v");
+const vTag = document.querySelector(".v-popup");
+
+toggleVTag.addEventListener("click", () => {
+  vTag.classList.toggle("show");
+  fTag.classList.remove("show");
+});
+
+const toggleFTag = document.querySelector(".toggle-f");
+const fTag = document.querySelector(".f-popup");
+
+toggleFTag.addEventListener("click", () => {
+  fTag.classList.toggle("show");
+  vTag.classList.remove("show");
+});
+
+//Change Background
+document.addEventListener("click", changeBground);
+var colors = {
+  taupe: "#483C32",
+  mocha: "#967969",
+  lightBrown: "#C4A484",
+  darkBrown: "#5C4033",
+  copper: "#B87333",
+  chocolate: "#7B3F00",
+  camel: "#C19A6B",
+  burntUmber: "#6E260E",
+};
+
+const colorKeys = Object.keys(colors);
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function changeBground() {
+  const randKey = getRandomInt(0, colorKeys.length);
+  console.log(randKey, colorKeys[randKey]);
+  const color = colors[colorKeys[randKey]];
+  document.body.style.backgroundColor = color;
+}
